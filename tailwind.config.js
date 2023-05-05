7f2e4d8bd816c9fdbde12b6d6e1b89f61dafb457
mertyldrr <mertyildirir14@gmail.com>
2023-05-06 01:56:29 +0200

    add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        lotrYellow: "#FFC107",
+        lotrBrown: "#795548",
+      },
       fontFamily: {
         aniron: "Aniron",
       },
