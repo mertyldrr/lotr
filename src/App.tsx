b9dd72a0732bd65dff2014083c2d00a824261064
mertyldrr <mertyildirir14@gmail.com>
2023-05-05 14:52:36 +0200

    create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Container } from "./components/Container";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Container />
+    </div>
+  );
+}
+
+export default App;
