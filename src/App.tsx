68c4bfc86bb1a2bbb7e40e56e04b4caa578a5f84
mertyldrr <mertyildirir14@gmail.com>
2023-05-06 01:57:30 +0200

    add routes, delete app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from "react";
-import "./App.css";
 import { Navbar } from "./components/Navbar";
-import { Container } from "./components/Container";
+import { Route, Routes } from "react-router-dom";
+import { Books } from "./components/Books";
+import { Movies } from "./components/Movies";
+import { Characters } from "./components/Characters";
+import { Quotes } from "./components/Quotes";
 
 function App() {
   return (
-    <div>
+    <div className="h-screen bg-black">
       <Navbar />
-      <Container />
+
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/quotes" element={<Quotes />} />
+      </Routes>
     </div>
   );
 }
