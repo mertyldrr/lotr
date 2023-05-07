b607859421e2f9b1866595b0ec58bd4b262cf71f
mertyldrr <mertyildirir14@gmail.com>
2023-05-07 23:50:22 +0200

    add constants for headers and order of headers

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,46 @@
 export const NavItems = ["books", "movies", "characters", "random quote"];
+export const BookHeaders = ["name"];
+export const MovieHeaders = [
+  "name",
+  "Academy Award Nominations",
+  "academy Award Wins",
+  "Box Office Revenue($M)",
+  "Budget($M)",
+  "Runtime In Minutes",
+  "Rotten Tomatoes Score",
+];
+export const CharacterHeaders = [
+  "name",
+  "gender",
+  "birth",
+  "spouse",
+  "race",
+  "death",
+  "Wiki",
+];
+
+export const CharacterHeaderOrder = [
+  "name",
+  "gender",
+  "birth",
+  "spouse",
+  "race",
+  "death",
+  "wikiUrl",
+];
+export const QuoteHeaderOrder = ["dialog", "character", "movie"];
+export const BookHeaderOrder = ["name"];
+export const MovieHeaderOrder = [
+  "name",
+  "academyAwardNominations",
+  "academyAwardWins",
+  "boxOfficeRevenueInMillions",
+  "budgetInMillions",
+  "runtimeInMinutes",
+  "rottenTomatoesScore",
+];
+
+export const QuoteHeaders = ["dialog", "character", "movie"];
+
+export const CharactersInPage = 8;
+export const QuotesInPage = 10;
