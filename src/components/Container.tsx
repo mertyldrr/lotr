91d62de6f806c1f0b0adb446f905a5f5663476dc
mertyldrr <mertyildirir14@gmail.com>
2023-05-07 23:50:01 +0200

    remove container component

--- src/components/Container.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import { fetchCharacters } from "../utils";
-
-export const Container = () => {
-  useEffect(() => {
-    fetchCharacters();
-  }, []);
-  return <div>TEST</div>;
-};
