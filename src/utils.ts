cfdb417ad3e514429a20900c5ea6a0b60e6bce97
mertyldrr <mertyildirir14@gmail.com>
2023-05-08 01:38:35 +0200

    add helper functions for data fetching

--- src/utils.ts
+++ src/utils.ts
@@ -1,38 +1,60 @@
 import axios from "axios";
+import { Book, Character, FilteredCharacter, Movie, Quote } from "./types";
 
-// TODO: fix types
-export const fetchBooks = async (): Promise<any> => {
-  const headers = {
-    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
-  };
-  return await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/book`, {
-    headers,
-  });
+const headers = {
+  Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
 };
 
-export const fetchMovies = async (): Promise<any> => {
-  const headers = {
-    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
-  };
-  return await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/movie`, {
+export const fetchBooks = async (): Promise<Book[]> => {
+  const res = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/book`, {
     headers,
   });
+  return res.data.docs as Book[];
 };
 
-export const fetchCharacters = async (): Promise<any> => {
-  const headers = {
-    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
-  };
-  return await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/character`, {
+export const fetchMovies = async (): Promise<Movie[]> => {
+  const res = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/movie`, {
     headers,
   });
+  return res.data.docs as Movie[];
 };
 
-export const fetchQuotes = async (): Promise<any> => {
-  const headers = {
-    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
-  };
-  return await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/quote`, {
-    headers,
-  });
+export const fetchCharacters = async (
+  limit: number = 10,
+  page: number = 1
+): Promise<FilteredCharacter[]> => {
+  const res = await axios.get(
+    `${process.env.REACT_APP_API_ENDPOINT}/character?limit=${limit}&page=${page}`,
+    {
+      headers,
+    }
+  );
+  const characters = res.data.docs as Character[];
+  const filteredCharacters: FilteredCharacter[] = characters.map(
+    ({ hair, realm, height, ...rest }) => rest
+  );
+  return filteredCharacters;
+};
+
+export const fetchQuotes = async (
+  limit: number = 10,
+  page: number = 1
+): Promise<Quote[]> => {
+  const res = await axios.get(
+    `${process.env.REACT_APP_API_ENDPOINT}/quote?limit=${limit}&page=${page}`,
+    {
+      headers,
+    }
+  );
+  return res.data.docs as Quote[];
+};
+
+export const fetchRandomQuote = async (id: string): Promise<Quote> => {
+  const res = await axios.get(
+    `${process.env.REACT_APP_API_ENDPOINT}/quote/${id}`,
+    {
+      headers,
+    }
+  );
+  return res.data.docs[0] as Quote;
 };
