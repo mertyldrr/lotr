aed05c2940e4912f15860c320343e60277667504
mertyldrr <mertyildirir14@gmail.com>
2023-05-07 23:49:11 +0200

    add bilbo font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      bilbo: ['"Bilbo"', ...defaultTheme.fontFamily.sans],
+    },
     extend: {
       colors: {
         lotrYellow: "#FFC107",
