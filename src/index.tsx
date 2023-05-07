fd175df797b77543db25f7adef17895fb8006e30
mertyldrr <mertyildirir14@gmail.com>
2023-05-08 01:36:49 +0200

    add browser router wrapper & normalize.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import "./input.css";
+import "normalize.css/normalize.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
